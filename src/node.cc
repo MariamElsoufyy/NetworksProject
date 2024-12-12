85554e6816c6b0194ca1ae0079787d8268ee53bf
MariamElsoufyy <mariamelsoufyx@gmail.com>
2024-12-12 15:30:50 +0200

    init2

---
+++ src/node.cc
@@ -0,0 +1,60 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "node.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include<bitset>
+using namespace std;
+Define_Module(Node);
+
+void Node::initialize()
+{
+
+
+
+}
+
+void Node::handleMessage(cMessage *msg)
+{
+    if(true)/// if it's coordinator msg
+    {
+        ifstream inputFile("node0.txt");
+           if(!inputFile.is_open()){
+               cout<<"Error in opening input file";
+               return;
+           }
+
+           string line;
+           while (getline(inputFile, line)) {
+
+               string pre = line.substr(0,4);
+               MessageData m;
+               m.prefix = bitset<4>(pre);
+               //////// prefix is written from right to left 4<------0
+                                            //prefix[0] -> Delay
+                                            //prefix[1] -> duplication
+                                            //prefix[2] -> Loss
+                                            //prefix[3] -> Modification
+               m.data = line.substr(5);
+               msgs.push_back(m);
+               }
+
+
+
+    }
+}
+//testtttt
---
+++ src/node.h
@@ -0,0 +1,47 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __NETWORKSPROJECT_NODE_H_
+#define __NETWORKSPROJECT_NODE_H_
+
+
+#include <omnetpp.h>
+#include <string>
+#include<bitset>
+#include<vector>
+
+using namespace omnetpp;
+using namespace std;
+
+struct MessageData{
+
+    string data;
+    bitset<4> prefix;  ////////written from right to left 4<------0
+                                  //prefix[0] -> Delay
+                                  //prefix[1] -> duplication
+                                  //prefix[2] -> Loss
+                                  //prefix[3] -> Modification
+};
+
+class Node : public cSimpleModule
+{
+private:
+    vector<MessageData> msgs;
+  protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+#endif
